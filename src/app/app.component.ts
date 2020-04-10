e78d9819262eb28faedabed4224dc0642cc39f22
DeBugger888 <wiktorsadowski.jd@gmail.com>
2020-04-10 16:47:44 +0200

    Added logic for generating password.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,35 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   generatedPassword = '';
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
 
   whenButtonClick() {
-    console.log(this.includeLetters);
-    this.generatedPassword = 'GENERATED PASSWORD';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    // const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += '!@#$%^&*()';
+    }
+
+    let randomPassword = '';
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+
+      randomPassword += validChars[index];
+    }
+    this.generatedPassword = randomPassword;
   }
 
   getPassword() {
@@ -21,7 +43,6 @@ export class AppComponent {
   }
 
   useLetters() {
-    console.log(this.includeLetters);
     this.includeLetters = !this.includeLetters;
   }
 
@@ -30,6 +51,14 @@ export class AppComponent {
   }
 
   useSymbols() {
-    this.includeNumbers = !this.includeSymbols;
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  setLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 }
